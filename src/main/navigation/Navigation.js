48245ee77b97d4d5019a4de12f3bfc181990eae3
sodojiwo <alvinadetya@rocketmail.com>
2019-01-09 00:29:56 +0700

    make selection and slider

--- src/main/navigation/Navigation.js
+++ src/main/navigation/Navigation.js
@@ -1,11 +1,76 @@
 import React, { Component } from 'react';
 import './Navigation.css';
+import Selection from './Selection';
+import Slider from './Slider';
+import Axios from 'axios';
 
 export default class Navigation extends Component {
+  state = {
+    genre: 'Comedy',
+    genres: [],
+    year: {
+      label: 'year',
+      min: 1990,
+      max: 2018,
+      step: 1,
+      value: { min: 2000, max: 2018 }
+    },
+    rating: {
+      label: 'rating',
+      min: 0,
+      max: 10,
+      step: 1,
+      value: { min: 8, max: 10 }
+    },
+    runtime: {
+      label: 'runtime',
+      min: 0,
+      max: 300,
+      step: 15,
+      value: { min: 60, max: 100 }
+    }
+  };
+
+  getGenres = () => {
+    Axios.get(
+      'https://api.themoviedb.org/3/genre/movie/list?api_key=4b79163ef3bf5048e4b25dbf42578ca3&language=en-US'
+    ).then(res => {
+      this.setState({
+        genres: res.data.genres
+      });
+      console.log(this.state.genres);
+    });
+  };
+
+  componentDidMount() {
+    this.getGenres();
+  }
+
+  onSliderChange = data => {
+    this.setState({
+      [data.type]: { ...this.state[data.type], value: data.value }
+    });
+  };
+
+  onGenreChange = event => {
+    this.setState({
+      genre: event.target.value
+    });
+  };
+
   render() {
+    const { genre, genres, year, rating, runtime } = this.state;
+    const { onGenreChange, onSliderChange } = this;
     return (
       <section className="navigation">
-        <p>Navigation</p>
+        <Selection
+          genre={genre}
+          genres={genres}
+          onGenreChange={onGenreChange}
+        />
+        <Slider data={year} onSliderChange={onSliderChange} />
+        <Slider data={rating} onSliderChange={onSliderChange} />
+        <Slider data={runtime} onSliderChange={onSliderChange} />
       </section>
     );
   }
--- src/main/navigation/Selection.js
+++ src/main/navigation/Selection.js
@@ -1,26 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './Selection.css';
 
-export default class Selection extends Component {
-  state = {
-    genre: 'comedy'
-  };
+const Selection = ({ genre, genres, onGenreChange }) => {
+  return (
+    <div className="selection">
+      <label>Genre</label>
+      <select value={genre} onChange={onGenreChange}>
+        {genres.map(genre => {
+          return (
+            <option value={genre.name} key={genre.id}>
+              {genre.name}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+};
 
-  onGenreChange = event => {
-    this.setState({
-      genre: event.target.value
-    });
-  };
-  render() {
-    const { genre } = this.state;
-    return (
-      <div className="selection">
-        <label>Genre</label>
-        <select value={genre}>
-          <option value="comedy">Comedy</option>
-          <option value="action">Action</option>
-          <option value="drama">Drama</option>
-        </select>
-      </div>
-    );
-  }
-}
+export default Selection;
